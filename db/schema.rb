d59e495f6821fe03e746b7c121c3234269013c73
Switch-Thomas <admiralswitch@gmail.com>
2015-08-11 15:44:08 -0700

    Updated ruby v, added timestamp to User migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150810215137) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
